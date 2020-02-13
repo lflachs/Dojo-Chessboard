decdf68eda345eecd2bdfd673370a4ec77778e96
Leonard <leonard.flachs@gmail.com>
2020-02-13 17:06:46 +0100

    add gitignore

--- chessboard.test.js
+++ chessboard.test.js
@@ -1,4 +1,5 @@
 const chessboard = require("./chessboard");
+const assert = require("assert");
 
 const expectedResult = [
   ["a - 1", "a - 2", "a - 3", "a - 4", "a - 5", "a - 6", "a - 7", "a - 8"],
